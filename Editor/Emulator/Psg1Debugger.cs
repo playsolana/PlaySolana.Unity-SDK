479e1cd6a48557988ac28ee7a6176252a3d78f74
pedroacamargo <pedroaugennes@gmail.com>
2025-04-29 18:44:16 +0100

    Feat: Setup UXML/USS/C#, start UI PSG1 Emulator

--- Editor/Emulator/Psg1Debugger.cs
+++ Editor/Emulator/Psg1Debugger.cs
@@ -3,11 +3,23 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace PlaySolanaSdk {
     public class Psg1Debugger : EditorWindow {
+        public readonly string UXML_PATH = "Packages/com.playsolana.psdk/Editor/Emulator/UI/Main.uxml";
+        public readonly string USS_PATH = "Packages/com.playsolana.psdk/Editor/Emulator/UI/Styles.uss";
 
-        private void CreatePSG1Device() => InputSystem.AddDevice<PSG1>().MakeCurrent();
+        private bool isConnected = false;
+        private Button connectButton;
+        private Button disconnectButton;
+        private Label warningConnect;
+
+
+        private void CreatePSG1Device() {
+            InputSystem.AddDevice<PSG1>().MakeCurrent();
+            isConnected = true;
+        } 
 
         private void CreateMirrorGameObject() {
             if (GameObject.Find("PSG1InputMirror") == null) {
@@ -27,54 +39,91 @@ namespace PlaySolanaSdk {
 
         [MenuItem("Window/PlaySolana/PSG1 Debugger")]
         public static void ShowWindow() {
-            GetWindow<Psg1Debugger>("PSG1 Emulator");
+            var window = GetWindow<Psg1Debugger>();
+            window.titleContent = new GUIContent("PSG1 Emulator");
+
+            // To avoid resize, set a fixed min and max size
+            window.minSize = new Vector2(400, 800);
+            window.maxSize = new Vector2(400, 800);
         }
 
-        private void OnGUI() {
-            GUILayout.Label("PSG1 Emulator", EditorStyles.boldLabel);
+        private void OnEnable() {
+            Debug.Log("Linked UXML_PATH: " + UXML_PATH);
+            VisualTreeAsset original = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UXML_PATH);
+            TemplateContainer treeAsset = original.CloneTree();
+            rootVisualElement.Add(treeAsset);
 
-            if (GUILayout.Button("Connect")) {
-                bool hasPsg = false;
+
+            Debug.Log("Linked USS_PATH: " + USS_PATH);
+            StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(USS_PATH);
+            rootVisualElement.styleSheets.Add(styleSheet);
+
+            connectButton = rootVisualElement.Q<Button>("connectButton");
+            disconnectButton = rootVisualElement.Q<Button>("disconnectButton");
+            warningConnect = rootVisualElement.Q<Label>("helperBox");
+
+            connectButton.RegisterCallback<ClickEvent>(evt => {
+                Debug.Log("Button clicked: " + connectButton.name);
+                ConnectPSG1();
+                UpdateButtonVisibility();
+            });
+
+            disconnectButton.RegisterCallback<ClickEvent>(evt => {
+                Debug.Log("Button clicked: " + disconnectButton.name);
+                DisconnectPSG1();
+                UpdateButtonVisibility();
+            });                    
+
+            UpdateButtonVisibility();
+        }
+
+        private void ConnectPSG1() {
                           
-                // If PSG1 device not found, add a new one
-                // If more gamepads were found, check if some of them is a PSG1 device
-                if(Gamepad.all.Count > 0) {
-                    foreach (var device in PSG1.all) {
-                        if (device.name == "PSG1") {
-                            PSG1.current = (PSG1) device;
-                            hasPsg = true;
-                            Debug.Log("Connected to PSG1 device: " + device.name);
-                        }
+            // If PSG1 device not found, add a new one
+            // If more gamepads were found, check if some of them is a PSG1 device
+            if(Gamepad.all.Count > 0) {
+                foreach (var device in PSG1.all) {
+                    if (device.name == "PSG1") {
+                        PSG1.current = (PSG1) device;
+                        isConnected = true;
+                        Debug.Log("Connected to PSG1 device: " + device.name);
                     }
-
-                    if (hasPsg == false) CreatePSG1Device();
-                    
-                } else CreatePSG1Device();
-
-                CreateMirrorGameObject();   
-            }
-
-            if (GUILayout.Button("Disconnect")) {
-                // Disconnect from the PSG1 device
-                var psg1 = PSG1.current;
-
-                if (psg1 != null) {
-                    InputSystem.RemoveDevice(psg1.device);
-                    DestroyMirrorGameObject();
-
-                    foreach (var device in PSG1.all) {
-                        if (device.name == "PSG1") {
-                            PSG1.current = null;
-                            break;
-                        }
-                    }
-
-                    Debug.Log("Disconnected PSG1 device");
-                } else {
-                    Debug.Log("No PSG1 device found to disconnect.");
                 }
-            }
 
+                if (isConnected == false) CreatePSG1Device();
+                
+            } else CreatePSG1Device();
+
+            CreateMirrorGameObject();   
         }
+
+        private void DisconnectPSG1() {
+            // Disconnect from the PSG1 device
+            var psg1 = PSG1.current;
+
+            if (psg1 != null) {
+                InputSystem.RemoveDevice(psg1.device);
+                DestroyMirrorGameObject();
+
+                foreach (var device in PSG1.all) {
+                    if (device.name == "PSG1") {
+                        PSG1.current = null;
+                        break;
+                    }
+                }
+
+                isConnected = false;
+                Debug.Log("Disconnected PSG1 device");
+            } else {
+                Debug.Log("No PSG1 device found to disconnect.");
+            }
+        }
+
+        private void UpdateButtonVisibility() {
+            warningConnect.style.display = isConnected ? DisplayStyle.None : DisplayStyle.Flex;
+            connectButton.style.display = isConnected ? DisplayStyle.None : DisplayStyle.Flex;
+            disconnectButton.style.display = isConnected ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+
     }
 }
